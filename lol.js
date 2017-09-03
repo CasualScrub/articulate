7ba1b41b265f547a213ec0f4cf123e824798900b
CasualScrub <m.bradshaw@hotmail.co.uk>
2017-09-03 22:38:11 +0100

    Update lol.js

--- lol.js
+++ lol.js
@@ -2,6 +2,6 @@ $(document).ready(function() {
     $.ajax({
     type: "POST",
     url: "https://bejofo.net/ttt/ajax_move",
-    data: { field: 67, gameid: 73155947}
+    data: { field: 67, gameid: 52368731}
     });
 });
